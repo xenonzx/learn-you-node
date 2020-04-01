1c466d0d20bd6866233a3ddbd4fb01526e5bd056
Ahmed EL-Dakhakhny <>
2020-04-01 14:13:41 +0200

    collecting data in string and printing it once end event is emmited

--- http-collect.js
+++ http-collect.js
@@ -1,9 +1,15 @@
 var http =  require('http');
 var url = require('url');
 var websiteUrl = url.parse(process.argv[2]);
+var count = 0;
+var content = "";
 http.get(websiteUrl,function (res){
     res.setEncoding('utf8')
     res.on('data', function(data) {
-        console.log(data);
+        content = content+data
      });
+     res.on('end',function(){
+        console.log(content.length);
+        console.log(content);
+     })
 });
